b5e5f2bcf06a1ffda3e0d11a2053987ea6433c1c
ptmsk <huy.tranacc13042@hcmut.edu.vn>
2022-05-29 09:49:23 +0700

    change the order of structure for easier desciption

--- constant.h
+++ constant.h
@@ -2,17 +2,17 @@
 #define CONSTANT_H_
 
 // configuration
-const float SPRITESIZE = 64.0;
+const int SPRITESIZE = 64;
 const int WINDOW_WIDTH = 1024;
 const int WINDOW_HEIGHT = 768;
 
 // block
-const int AIR = 0;
+const int GROUND = 0;
 const int WALL = 1;
-const int PLAYER = 2;
-const int BOX = 3;
-const int TARGET = 4;
-const int TARGET_FILLED = 5;
+const int BOX = 2;
+const int TARGET = 3;
+const int TARGET_FILLED = 4;
+const int PLAYER = 5;
 
 enum Direction {
     UP,
